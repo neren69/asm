265f5ee02feb52d4cc166a25e68cf9cc2c95200a
neren69 <newtest.test01234@gmail.com>
2025-11-10 08:30:06 +0600

    Delete AssemblySolutions-main directory

--- AssemblySolutions-main/[19-20] Mid - 05.asm
+++
@@ -1,6 +0,0 @@
-; 512Ch + 4185h 
-; signed or unsigned overflow?
-
-MOV AX, 512Ch
-MOV BX, 4185h
-ADD AX, BX
--- AssemblySolutions-main/[19-20] Mid - 06.asm
+++
@@ -1,6 +0,0 @@
-; 2143h - 1986h
-; signed or unsigned overflow?
-
-MOV AX, 2143h
-MOV BX, 1986h
-SUB AX, BX
